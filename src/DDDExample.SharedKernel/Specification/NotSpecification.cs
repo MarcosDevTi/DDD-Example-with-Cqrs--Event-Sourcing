8830839bc0f47dd7cfd03f564c130fd65cb50781
Marcos Souza <marcos.dev.ti@gmail.com>
2018-08-04 00:23:55 -0300

    Refactory

---
+++ src/DDDExample.SharedKernel/Specification/NotSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DDDExample.SharedKernel.Specification
+{
+    internal sealed class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _specification;
+
+        public NotSpecification(Specification<T> specification)
+        {
+            _specification = specification;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> expression = _specification.ToExpression();
+            UnaryExpression notExpression = Expression.Not(expression.Body);
+
+            return Expression.Lambda<Func<T, bool>>(notExpression, expression.Parameters.Single());
+        }
+    }
+}
